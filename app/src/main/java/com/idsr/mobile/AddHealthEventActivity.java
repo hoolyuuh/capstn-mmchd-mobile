083cf3433c142a32f73366fba7b579319dc3909d
shannongailhoo <shannon_gail_ho@dlsu.edu.ph>
2022-08-03 23:21:20 +0800

    M0: fixed more stuff - errors during build

--- app/src/main/java/com/idsr/mobile/AddHealthEventActivity.java
+++ app/src/main/java/com/idsr/mobile/AddHealthEventActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.idsr.mobile.databinding.ActivityHomeBinding;
+
 public class AddHealthEventActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+//        binding = ActivityAddHealthEventBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_health_event1);
     }
 }
